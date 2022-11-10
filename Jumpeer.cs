14a283840fe951e3e2ebf78bd33d140b3b5b3757
nimax <nimax29@gmail.com>
2022-11-10 09:42:26 -0700

    jumper added

---
+++ Jumpeer.cs
@@ -0,0 +1,21 @@
+using System;
+public class Jumpeer
+    {
+        public Jumpeer(){
+
+        }
+        public void createJumper()
+        {
+            Console.WriteLine("  --- ");
+            Console.WriteLine(" /___\\");
+            Console.WriteLine(" \\   /");
+            Console.WriteLine("  \\ /");
+            Console.WriteLine("   O  ");
+            Console.WriteLine(" / | \\");                      
+            Console.WriteLine(" /   \\");
+            Console.WriteLine("");
+            Console.WriteLine("^^^^^^^^");
+
+        }
+
+    }
