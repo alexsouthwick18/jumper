4999305c955966767301a1edc6daef9b286be0b9
nimax <nimax29@gmail.com>
2022-11-10 10:12:52 -0700

    Merge branch 'main' of https://github.com/alexsouthwick18/jumper

--- Director.cs
+++ Director.cs
@@ -11,28 +11,7 @@ class Director
         while (keepPlaying)
         {
             
-            GetInputs();
-            DoUpdates();
-            DoOutputs();
         }
     }
   
-
-    public void GetInputs()
-    {   
-        Console.WriteLine("Guess a letter [A-Z]: ");
-        Console.ReadKey();
-        
-    }
-    
-    public void DoUpdates()
-    {
-        
-       
-     
-    }
-    public void DoOutputs()
-    {
-        
-    }
 }
