2866496d797491a084d0f893052e3930d43d5c0d
nimax <nimax29@gmail.com>
2022-11-14 10:19:20 -0700

    list.cs

--- list.cs
+++ list.cs
@@ -7,18 +7,20 @@ namespace Jumper
     class chosenWordList
     {
         
-        private string[] listWords = { "Apple", "Mouse", "Index", "Dozen", "Union" };
+        private string[] listWords = { "apple", "mouse", "index", "dozen", "union" };
         public List<string> getWordList(){
 
             List<string> wordsList = new List<string>(listWords);
             return wordsList;
+
             
         }
         public void GetWord()
         {
+            List<string> listy = getWordList();
             var random = new Random();
-            int index = random.Next(wordslist.Count);
-            Console.WriteLine(wordslist[index]);
+            int index = random.Next(listy.Count);
+            Console.WriteLine(listy[index]);
         }
     }
 }
