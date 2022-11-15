ae1462d54ec772f4bf99e2202926feed6c70fe1c
Brendan Willis <brendanwillis@me.com>
2022-11-15 08:48:38 -0700

    Word - for get input

---
+++ Word.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Jumper
+{
+    public class Word
+    {
+       private string secretWord; 
+       private string hint; 
+
+        public string _word()
+        {
+
+
+            return secretWord;
+        }
+
+        public void CompareToGuess(string w)
+        {  
+            string hint = "_";
+            int attempts = 4;  
+
+            while(attempts > 0)
+            {
+                if('Z'.CompareTo('Z') == 0)
+                    Console.WriteLine("Same character !");
+                if (guess == secretWord)
+            }
+        }
+        public string GetHint()
+        {
+            return hint;
+        }
+        public bool Guessed()
+        {
+            return(hint == secretWord);
+        }
+    }
+
+}
